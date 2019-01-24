617977cf85ee64ebace748c4db8af40abc3ddc0d
Chris Duryee <cduryee@redhat.com>
2019-01-24 15:33:58 -0500

    fix returned json

--- app.py
+++ app.py
@@ -6,24 +6,28 @@ CORS(app)
 
 PHONY_DATA = {'packages': [{'name': 'gcc',
                             'status': 'SAME',
-                            'hosts': ['8.2.1-6.fc29.x86_64', '8.2.1-6.fc29.x86_64']},
+                            'hosts': [{'host1.example.com': '8.2.1-6.fc29.x86_64'},
+                                      {'host2.example.com': '8.2.1-6.fc29.x86_64'}]},
                            {'name': 'gcc-c++',
                             'status': 'SAME',
-                            'hosts': ['8.2.1-6.fc29.x86_64', '8.2.1-6.fc29.x86_64']},
+                            'hosts': [{'host1.example.com': '8.2.1-6.fc29.x86_64'},
+                                      {'host2.example.com': '8.2.1-6.fc29.x86_64'}]},
                            {'name': 'gcc-gdb-plugin',
                             'status': 'SAME',
-                            'hosts': ['8.2.1-6.fc29.x86_64', '8.2.1-6.fc29.x86_64']},
+                            'hosts': [{'host1.example.com': '8.2.1-6.fc29.x86_64'},
+                                      {'host2.example.com': '8.2.1-6.fc29.x86_64'}]},
                            {'name': 'robert_burns',
                             'status': 'UNKNOWN',
-                            'hosts': ['1.0-0.el99.noarch', None]},
+                            'hosts': [{'host1.example.com': '1.0-1.el99.noarch'},
+                                      {'host2.example.com': None}]},
                            ],
-              'metadata': {'hosts': ['host1.example.com', 'host2.example.com']},
               'facts': [{'name': 'uname.release',
                          'status': 'DIFF',
-                         'hosts': ['4.19.10-300.fc29.x86_64', '4.19.9-300.fc29.x86_64']},
+                         'hosts': [{'host1.example.com': '4.19.10-300.fc29.x86_64'},
+                                   {'host2.example.com': '4.19.9-300.fc29.x86_64'}]},
                         {'name': 'lscpu.socket(s)',
                          'status': 'SAME',
-                         'hosts': ['1', '1']}]}
+                         'hosts': [{'host1.example.com': '1'}, {'host2.example.com': '1'}]}]}
 
 
 @app.route("/compare")
