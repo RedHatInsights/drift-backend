b0d75e1bebb20e07849ce971a25a54031cb8828c
Chris Duryee <cduryee@redhat.com>
2019-05-03 13:31:35 -0400

    allow configuration of thread count

--- run_app.sh
+++ run_app.sh
@@ -12,6 +12,10 @@ if [ -z "$NUM_WORKERS" ];
   then NUM_WORKERS=2
 fi
 
-prometheus_multiproc_dir=$TEMPDIR gunicorn wsgi -w $NUM_WORKERS -b 0.0.0.0:8080 --log-level=$LOG_LEVEL --access-logfile=- --config ./gunicorn.conf.py
+if [ -z "$THREADS" ];
+  then THREADS=2
+fi
+
+prometheus_multiproc_dir=$TEMPDIR gunicorn wsgi -w $NUM_WORKERS --threads $THREADS -b 0.0.0.0:8080 --log-level=$LOG_LEVEL --access-logfile=- --config ./gunicorn.conf.py
 
 rm -rf $TEMPDIR
