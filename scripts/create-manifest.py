41a981e7ae0766bb76df48cac1f07480461656e7
Chris Duryee <cduryee@redhat.com>
2019-10-08 17:24:01 -0400

    bump kerlescan to 0.3

--- scripts/create-manifest.py
+++ scripts/create-manifest.py
@@ -8,7 +8,7 @@ with open("Pipfile.lock") as json_file:
     lockfile = json.load(json_file)
 
 with open("drift-manifest", "w") as manifest:
-    for name, value in lockfile["default"].items():
+    for name, value in sorted(lockfile["default"].items()):
         if "version" in value:
             version = value["version"].replace("=", "")
             manifest.write("drift/python:3.6=%s:%s\n" % (name, version))
