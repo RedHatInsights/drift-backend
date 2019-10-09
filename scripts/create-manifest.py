65e15c8b60b69f731a56250045e1dd714d9904b1
Chris Duryee <cduryee@redhat.com>
2019-10-09 09:52:12 -0400

    bump kerlescan to 0.3
    I don't think system-baseline uses any of the changes in 0.3, but I
    wanted to bump anyway to keep it level with drift service.

--- scripts/create-manifest.py
+++ scripts/create-manifest.py
@@ -8,7 +8,7 @@ with open("Pipfile.lock") as json_file:
     lockfile = json.load(json_file)
 
 with open("system_baseline-manifest", "w") as manifest:
-    for name, value in lockfile["default"].items():
+    for name, value in sorted(lockfile["default"].items()):
         if "version" in value:
             version = value["version"].replace("=", "")
             manifest.write("system_baseline/python:3.6=%s:%s\n" % (name, version))
