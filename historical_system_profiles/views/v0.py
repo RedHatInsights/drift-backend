c40784825d8dbaf8a5f9726748260427600bbfc8
Chris Duryee <cduryee@redhat.com>
2019-12-16 15:12:16 -0500

    feature: add ability to fetch profiles
    This commit adds the ability to fetch individual profiles. It is still
    mock data for now, but the mocks are moving further down the stack.

--- historical_system_profiles/views/v0.py
+++ historical_system_profiles/views/v0.py
@@ -12,6 +12,16 @@ def get_version():
     return {"version": "0.0.1"}
 
 
+def get_pits_by_ids(profile_ids):
+    """
+    return a list of historical system profiles for the given profile IDs
+    """
+    if profile_ids == ["100"]:
+        return mocks.MOCK_PROFILE_RESPONSE
+    else:
+        return {"results": []}
+
+
 def get_pits_by_inventory_id(inventory_ids):
     """
     return a list of historical system profiles for a given inventory id
