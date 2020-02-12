9a90ac40fbe08cb906117950848fbde10f55e356
Chris Duryee <cduryee@redhat.com>
2020-02-12 16:04:41 -0500

    feature: add RBAC support
    This commit adds RBAC support. It also adds the usual other request
    checks that we do.

--- tests/test_v0_api.py
+++ tests/test_v0_api.py
@@ -4,11 +4,18 @@ from historical_system_profiles import app
 
 import unittest
 import mock
+from mock import patch
 from . import fixtures
 
 
-class ApiTests(unittest.TestCase):
+class ApiTest(unittest.TestCase):
     def setUp(self):
+        self.rbac_patcher = patch(
+            "historical_system_profiles.views.v0.view_helpers.ensure_has_role"
+        )
+        patched_rbac = self.rbac_patcher.start()
+        patched_rbac.return_value = None  # validate all RBAC requests
+        self.addCleanup(self.stopPatches)
         test_connexion_app = app.create_app()
         test_flask_app = test_connexion_app.app
         self.client = test_flask_app.test_client()
@@ -22,6 +29,8 @@ class ApiTests(unittest.TestCase):
     def stopPatches(self):
         self.rbac_patcher.stop()
 
+
+class HSPApiTests(ApiTest):
     def test_status_bad_uuid(self):
         response = self.client.get(
             "/api/historical-system-profiles/v0/profiles/1",
