9000bfc15827b32481c439d5d6fd1d5a6c7a4a5e
Dana Walker <dawalker@redhat.com>
2020-04-29 10:22:47 -0400

    fix: check hsp exists, refactor

---
+++ kerlescan/hsp_service_interface.py
@@ -0,0 +1,33 @@
+from urllib.parse import urljoin
+
+from kerlescan import config
+from kerlescan.inventory_service_interface import _ensure_correct_system_count
+from kerlescan.service_interface import fetch_data
+from kerlescan.constants import AUTH_HEADER_NAME, HSP_SVC_ENDPOINT
+
+
+def fetch_historical_sys_profiles(
+    historical_sys_profile_ids, service_auth_key, logger, counters
+):
+    """
+    fetch historical system profiles
+    """
+
+    auth_header = {AUTH_HEADER_NAME: service_auth_key}
+
+    historical_sys_profile_location = urljoin(config.hsp_svc_hostname, HSP_SVC_ENDPOINT)
+
+    historical_sys_profile_result = fetch_data(
+        historical_sys_profile_location,
+        auth_header,
+        historical_sys_profile_ids,
+        logger,
+        counters["hsp_service_requests"],
+        counters["hsp_service_exceptions"],
+    )
+
+    _ensure_correct_system_count(
+        historical_sys_profile_ids, historical_sys_profile_result
+    )
+
+    return historical_sys_profile_result
