77f59cd7b252a17ea52def9695f8caee5ecd0feb
Chris Duryee <cduryee@redhat.com>
2020-05-05 16:27:25 -0400

    add deleter metrics
    This commit invokes the incrementers for delete operations.

--- historical_system_profiles/deleter.py
+++ historical_system_profiles/deleter.py
@@ -1,4 +1,5 @@
 from historical_system_profiles import db_interface
+from historical_system_profiles import listener_metrics as metrics
 
 
 def _delete_profiles(data, ptc, logger):
@@ -8,6 +9,15 @@ def _delete_profiles(data, ptc, logger):
     inventory_id = data.value["id"]
     request_id = data.value["request_id"]
     account = data.value["account"]
+
+    _record_recv_message(request_id, inventory_id, account, ptc)
+    db_interface.delete_hsps_by_inventory_id(inventory_id)
+    logger.info("deleted profiles for inventory_id %s" % inventory_id)
+    _record_success_message(request_id, inventory_id, account, ptc)
+
+
+def _record_recv_message(request_id, inventory_id, account, ptc):
+    metrics.delete_messages_consumed.inc()
     ptc.emit_received_message(
         "received inventory delete event",
         request_id=request_id,
@@ -15,8 +25,9 @@ def _delete_profiles(data, ptc, logger):
         inventory_id=inventory_id,
     )
 
-    db_interface.delete_hsps_by_inventory_id(inventory_id)
-    logger.info("deleted profiles for inventory_id %s" % inventory_id)
+
+def _record_success_message(request_id, inventory_id, account, ptc):
+    metrics.delete_messages_processed.inc()
     ptc.emit_success_message(
         "deleted profiles for inventory record",
         request_id=request_id,
@@ -30,6 +41,7 @@ def _emit_delete_error(data, ptc):
     send an error message to payload tracker. This does not raise an
     exception.
     """
+    metrics.delete_messages_errored.inc()
     inventory_id = data.value["id"]
     request_id = data.value["request_id"]
     account = data.value["account"]
