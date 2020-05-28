a4784390cf9506ec8b3818f32d19cee85405a4f5
Dana Walker <dawalker@redhat.com>
2020-05-28 13:15:08 -0400

    remove lead _ of ensure_correct_system_count

--- kerlescan/hsp_service_interface.py
+++ kerlescan/hsp_service_interface.py
@@ -1,7 +1,7 @@
 from urllib.parse import urljoin
 
 from kerlescan import config
-from kerlescan.inventory_service_interface import _ensure_correct_system_count
+from kerlescan.inventory_service_interface import ensure_correct_system_count
 from kerlescan.service_interface import fetch_data
 from kerlescan.constants import AUTH_HEADER_NAME, HSP_SVC_ENDPOINT
 
@@ -26,7 +26,7 @@ def fetch_historical_sys_profiles(
         counters["hsp_service_exceptions"],
     )
 
-    _ensure_correct_system_count(
+    ensure_correct_system_count(
         historical_sys_profile_ids, historical_sys_profile_result
     )
 
