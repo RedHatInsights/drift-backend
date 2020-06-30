979435530f85e3f9a1cf1d99dccfbadcd1f6c570
Chris Duryee <cduryee@redhat.com>
2020-06-30 16:28:44 -0400

    small refactor when fetching inventory data
    This commit breaks parsing logic out of the API fetch to inventory and
    into its own function.
    
    This also removes a counter for a the "systems without profiles"
    metric. This was needed during Phase 1, but is no longer relevant.

--- kerlescan/inventory_service_interface.py
+++ kerlescan/inventory_service_interface.py
@@ -22,6 +22,56 @@ def ensure_correct_system_count(system_ids_requested, result):
         )
 
 
+def interleave_systems_and_profiles(systems_result, system_profiles_result):
+    """
+    given a system result and system profile result, interleave them and create
+    records suitable for a drift comparison.
+    """
+    # create a blank profile for each system
+    system_profiles = {
+        system["id"]: {"system_profile": {}} for system in systems_result
+    }
+    # update with actual profile info if we have it
+    for profile in system_profiles_result:
+        system_profiles[profile["id"]] = profile
+
+    systems_without_profile_count = 0
+    # fill in any fields that were not on the profile
+    for system_id in system_profiles:
+        # before we populate the fields, mark where the system profile came
+        # from. This is useful so we know if the system has uploaded a tarball
+        # or not.
+        if system_profiles[system_id]["system_profile"]:
+            system_profiles[system_id]["system_profile"]["system_profile_exists"] = True
+        else:
+            system_profiles[system_id]["system_profile"][
+                "system_profile_exists"
+            ] = False
+            systems_without_profile_count += 1
+        # TODO: populate more than just integers and strings
+        for key in SYSTEM_PROFILE_INTEGERS | SYSTEM_PROFILE_STRINGS:
+            if key not in system_profiles[system_id]["system_profile"]:
+                system_profiles[system_id]["system_profile"][key] = "N/A"
+
+    systems_with_profiles = []
+    for system in systems_result:
+        system_with_profile = system
+        # we do not use the 'facts' field
+        system_with_profile.pop("facts", None)
+
+        system_with_profile["system_profile"] = system_profiles[system["id"]][
+            "system_profile"
+        ]
+        # we duplicate a bit of metadata in the inner dict to make parsing easier
+        system_with_profile["system_profile"]["id"] = system["id"]
+        system_with_profile["system_profile"]["fqdn"] = system["fqdn"]
+        system_with_profile["system_profile"]["updated"] = system["updated"]
+
+        systems_with_profiles.append(system_with_profile)
+
+    return systems_with_profiles
+
+
 def fetch_systems_with_profiles(system_ids, service_auth_key, logger, counters):
     """
     fetch systems from inventory service
@@ -56,49 +106,4 @@ def fetch_systems_with_profiles(system_ids, service_auth_key, logger, counters):
 
     ensure_correct_system_count(system_ids, systems_result)
 
-    # create a blank profile for each system
-    system_profiles = {
-        system["id"]: {"system_profile": {}} for system in systems_result
-    }
-    # update with actual profile info if we have it
-    for profile in system_profiles_result:
-        system_profiles[profile["id"]] = profile
-
-    systems_without_profile_count = 0
-    # fill in any fields that were not on the profile
-    for system_id in system_profiles:
-        # before we populate the fields, mark where the system profile came
-        # from. This is useful so we know if the system has uploaded a tarball
-        # or not.
-        if system_profiles[system_id]["system_profile"]:
-            system_profiles[system_id]["system_profile"]["system_profile_exists"] = True
-        else:
-            system_profiles[system_id]["system_profile"][
-                "system_profile_exists"
-            ] = False
-            systems_without_profile_count += 1
-        # TODO: populate more than just integers and strings
-        for key in SYSTEM_PROFILE_INTEGERS | SYSTEM_PROFILE_STRINGS:
-            if key not in system_profiles[system_id]["system_profile"]:
-                system_profiles[system_id]["system_profile"][key] = "N/A"
-
-    # record how many no-profile systems were in this report
-    counters["systems_compared_no_sysprofile"].observe(systems_without_profile_count)
-
-    systems_with_profiles = []
-    for system in systems_result:
-        system_with_profile = system
-        # we do not use the 'facts' field
-        system_with_profile.pop("facts", None)
-
-        system_with_profile["system_profile"] = system_profiles[system["id"]][
-            "system_profile"
-        ]
-        # we duplicate a bit of metadata in the inner dict to make parsing easier
-        system_with_profile["system_profile"]["id"] = system["id"]
-        system_with_profile["system_profile"]["fqdn"] = system["fqdn"]
-        system_with_profile["system_profile"]["updated"] = system["updated"]
-
-        systems_with_profiles.append(system_with_profile)
-
-    return systems_with_profiles
+    return interleave_systems_and_profiles(systems_result, system_profiles_result)
