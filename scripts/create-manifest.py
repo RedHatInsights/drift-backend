97876dd6b380c73bf729c816cc00cd5f3d7bf0d9
Dana Walker <dawalker@redhat.com>
2020-07-20 13:33:56 -0400

    update manifest format

--- scripts/create-manifest.py
+++ scripts/create-manifest.py
@@ -11,9 +11,13 @@ with open("system_baseline-manifest", "w") as manifest:
     for name, value in sorted(lockfile["default"].items()):
         if "version" in value:
             version = value["version"].replace("=", "")
-            manifest.write("system_baseline/python:3.6=%s:%s\n" % (name, version))
+            manifest.write(
+                "services-drift:system_baseline/python:3.6=%s:%s\n" % (name, version)
+            )
         elif "ref" in value:
             ref = value["ref"]
-            manifest.write("system_baseline/python:3.6=%s:%s\n" % (name, ref))
+            manifest.write(
+                "services-drift:system_baseline/python:3.6=%s:%s\n" % (name, ref)
+            )
         else:
             raise "unable to parse %s" % value
