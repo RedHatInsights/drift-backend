534da1dd579a9fbeac6c7e5682cc5314d7463f8e
Dana Walker <dawalker@redhat.com>
2020-07-27 17:57:28 -0400

    update to python 3.8

--- client/setup.sh
+++ client/setup.sh
@@ -1,9 +1,9 @@
-dnf install -y git-core autoconf automake pkg-config python36 wget binutils make zip
+dnf install -y git-core autoconf automake pkg-config python38 wget binutils make zip
 git clone https://github.com/RedHatInsights/insights-client.git
 git clone https://github.com/RedHatInsights/insights-core.git
 cd insights-client/
 ./autogen.sh 
-mkdir -p /usr/local/lib/python3.6/site-packages/
+mkdir -p /usr/local/lib/python3.8/site-packages/
 ln -s /usr/bin/python3 /usr/bin/python
 make
 mkdir /etc/insights-client
