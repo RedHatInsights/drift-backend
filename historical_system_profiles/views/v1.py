78281f42185cf35036a888d3e5789c983498f314
Dana Walker <dawalker@redhat.com>
2021-02-24 10:47:03 -0500

    Add standalone hsp rbac

--- historical_system_profiles/views/v1.py
+++ historical_system_profiles/views/v1.py
@@ -182,7 +182,7 @@ def ensure_account_number():
 @section.before_app_request
 def ensure_rbac():
     return view_helpers.ensure_has_permission(
-        permissions=["drift:*:*", "drift:baselines:read"],
+        permissions=["drift:*:*", "drift:historical-system-profiles:read"],
         application="drift",
         app_name="historical-system-profiles",
         request=request,
