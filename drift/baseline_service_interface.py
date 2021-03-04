f2fc6a17f83177dd1f57c604c5b3f6b031606726
Petr Blaho <petrblaho@redhat.com>
2021-03-04 16:07:39 +0100

    DRFT-199 Adds audit logging

--- drift/baseline_service_interface.py
+++ drift/baseline_service_interface.py
@@ -1,5 +1,7 @@
 from urllib.parse import urljoin
 
+from flask import request, current_app
+
 from drift import metrics
 from kerlescan import config
 from kerlescan.service_interface import fetch_data
@@ -15,6 +17,8 @@ def fetch_baselines(baseline_ids, service_auth_key, logger):
 
     baseline_location = urljoin(config.baseline_svc_hostname, BASELINE_SVC_ENDPOINT)
 
+    message = "reading baselines"
+    current_app.logger.audit(message, request=request)
     baseline_result = fetch_data(
         baseline_location,
         auth_header,
