28c8cd2850abea3245afd39282148187a18fc7d1
Petr Blaho <petrblaho@redhat.com>
2021-04-19 13:50:03 +0200

    Changes baseline API call to conform with current implementation of API

--- historical_system_profiles/baseline_service_interface.py
+++ historical_system_profiles/baseline_service_interface.py
@@ -2,7 +2,7 @@ from urllib.parse import urljoin
 
 from historical_system_profiles import metrics
 from kerlescan import config
-from kerlescan.service_interface import fetch_data
+from kerlescan.service_interface import fetch_url, internal_auth_header
 from kerlescan.constants import AUTH_HEADER_NAME, INTERNAL_BASELINE_SVC_ENDPOINT
 
 
@@ -12,17 +12,19 @@ def fetch_system_baseline_associations(system_id, service_auth_key, logger):
     this system is associated with
     """
 
-    auth_header = {AUTH_HEADER_NAME: service_auth_key}
+    auth_header = {**{AUTH_HEADER_NAME: service_auth_key}, **internal_auth_header()}
 
     internal_baselines_location = urljoin(
         config.baseline_svc_hostname, INTERNAL_BASELINE_SVC_ENDPOINT
     )
 
-    return fetch_data(
-        internal_baselines_location,
+    query = "?system_id=%s" % system_id
+    url = internal_baselines_location % query
+    result =  fetch_url(
+        url,
         auth_header,
-        system_id,
         logger,
         metrics.baseline_service_requests,
         metrics.baseline_service_exceptions,
     )
+    return result
