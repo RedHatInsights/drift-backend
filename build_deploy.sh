41d2c8c0f4c2ed25c3a2e76363283b13a06718df
Mike Sager <msager@redhat.com>
2021-05-20 22:55:59 -0400

    Add build_deploy.sh and pr_check.sh as part of clowder migration

---
+++ build_deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -exv
+
+IMAGE_NAME="quay.io/cloudservices/drift-backend"
+IMAGE_TAG=$(git rev-parse --short=7 HEAD)
+
+if [[ -z "$QUAY_USER" || -z "$QUAY_TOKEN" ]]; then
+    echo "QUAY_USER and QUAY_TOKEN must be set"
+    exit 1
+fi
+
+DOCKER_CONF="$PWD/.docker"
+mkdir -p "$DOCKER_CONF"
+
+docker --config="$DOCKER_CONF" login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
+docker --config="$DOCKER_CONF" login -u="$RH_REGISTRY_USER" -p="$RH_REGISTRY_TOKEN" registry.redhat.io
+docker --config="$DOCKER_CONF" build -t "${IMAGE_NAME}:${IMAGE_TAG}" .
+docker --config="$DOCKER_CONF" push "${IMAGE_NAME}:${IMAGE_TAG}"
+
+# To enable backwards compatibility with ci, qa, and smoke, always push latest and qa tags
+
+for TAG in "latest" "qa"; do
+    docker --config="$DOCKER_CONF" tag "${IMAGE_NAME}:${IMAGE_TAG}" "${IMAGE_NAME}:$TAG"
+    docker --config="$DOCKER_CONF" push "${IMAGE_NAME}:$TAG"
+done
