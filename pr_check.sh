10fdb5c95bb6ce70a82b535d7d524fe9b01d0e24
Mike Sager <msager@redhat.com>
2021-06-01 10:15:05 -0400

    Add dummy test result

--- pr_check.sh
+++ pr_check.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 set -exv
+
+# Make directory for artifacts
+mkdir -p artifacts
+
+cat << EOF > artifacts/junit-dummy.xml
+<testsuite tests="1">
+    <testcase classname="dummy" name="dummytest"/>
+</testsuite>
+EOF
