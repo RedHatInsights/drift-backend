ff95082a04571b204d59cd874decb6724524dfb9
Dana Walker <dawalker@redhat.com>
2021-07-14 16:30:13 -0400

    bump kerlescan 0.74

--- tests/test_info_parser.py
+++ tests/test_info_parser.py
@@ -11,10 +11,8 @@ class InfoParserTests(unittest.TestCase):
             "0:bluejeans-1.37.22-1": ("bluejeans", "1.37.22-1"),
             "0:brscan4-0.4.7-1": ("brscan4", "0.4.7-1"),
             "0:epel-release-7-11": ("epel-release", "7-11"),
-            "0:gpg-pubkey-fd431d51-4ae0493b": ("gpg-pubkey", "fd431d51-4ae0493b"),
             "0:hll2395dwpdrv-4.0.0-1": ("hll2395dwpdrv", "4.0.0-1"),
             "0:skypeforlinux-8.34.0.78-1": ("skypeforlinux", "8.34.0.78-1"),
-            "0:gpg-pubkey-f4a80eb5-53a7ff4b": ("gpg-pubkey", "f4a80eb5-53a7ff4b"),
             "no_epoch-1-1": ("no_epoch", "1-1"),
         }
 
