4c421c758ac24d3e041495cce49098bb4fc63e0a
Joao Paulo Ramos <joao.oramos11@gmail.com>
2021-08-05 15:58:40 -0300

    Clowderized system-baseline service

--- run_app.sh
+++ run_app.sh
@@ -1,24 +1,40 @@
 #!/usr/bin/env bash
 
-TEMPDIR=`mktemp -d`
+if [ -z "$ACG_CONFIG"]; then
+  echo "Did not found ACG_CONFIG - RUNNING LOCALLY"
 
-if [ -z "$LOG_LEVEL" ];
-  then LOG_LEVEL='info'
+  TEMPDIR=`mktemp -d`
+
+  if [ -z "$LOG_LEVEL" ];
+    then LOG_LEVEL='info'
+  fi
+
+  if [ -z "$NUM_WORKERS" ];
+    then NUM_WORKERS=2
+  fi
+
+  if [ -z "$THREADS" ];
+    then THREADS=2
+  fi
+
+  if [ -z "$PORT" ];
+    then PORT=8080
+  fi
+
+  prometheus_multiproc_dir=$TEMPDIR gunicorn wsgi -w $NUM_WORKERS --threads $THREADS -b 0.0.0.0:$PORT --log-level=$LOG_LEVEL --access-logfile=- --config ./gunicorn.conf.py
+
+  rm -rf $TEMPDIR
+else
+  echo "Found ACG_CONFIG - RUNNING WITH CLOWDER"
+  if [ -z "$IS_MIGRATION" ]; then
+      echo "RUNNING SYSTEM BASELINE SERVICE"
+      PORT=8000
+      APP_CONFIG='gunicorn.conf.py'
+      exec gunicorn wsgi --bind=0.0.0.0:$PORT --access-logfile=- --config "$APP_CONFIG"
+  else
+    echo "RUNNING DB MIGRATION"
+    PORT=8000
+    APP_CONFIG='gunicorn.conf.py'
+    bash -c "FLASK_APP=system_baseline.app:get_flask_app_with_migration flask db upgrade"
+  fi
 fi
-
-if [ -z "$NUM_WORKERS" ];
-  then NUM_WORKERS=2
-fi
-
-if [ -z "$THREADS" ];
-  then THREADS=2
-fi
-
-if [ -z "$PORT" ];
-  then PORT=8080
-fi
-
-prometheus_multiproc_dir=$TEMPDIR gunicorn wsgi -w $NUM_WORKERS --threads $THREADS -b 0.0.0.0:$PORT --log-level=$LOG_LEVEL --access-logfile=- --config ./gunicorn.conf.py
-
-
-rm -rf $TEMPDIR
--- system_baseline/db_config.py
+++ system_baseline/db_config.py
@@ -1,12 +1,23 @@
 import os
 
+from app_common_python import LoadedConfig, isClowderEnabled
+
+
+def load_db_setting(env_name, attribute, default):
+    if isClowderEnabled():
+        cfg = LoadedConfig
+
+        return vars(cfg.database)[attribute]
+
+    return os.getenv(env_name, default)
+
 
 # please ensure these are all documented in README.md
 
-_db_user = os.getenv("BASELINE_DB_USER", "insights")
-_db_password = os.getenv("BASELINE_DB_PASS", "insights")
-_db_host = os.getenv("BASELINE_DB_HOST", "localhost")
-_db_name = os.getenv("BASELINE_DB_NAME", "baselinedb")
+_db_user = load_db_setting("BASELINE_DB_USER", "username", "insights")
+_db_password = load_db_setting("BASELINE_DB_PASS", "password", "insights")
+_db_host = load_db_setting("BASELINE_DB_HOST", "hostname", "localhost")
+_db_name = load_db_setting("BASELINE_DB_NAME", "name", "baselinedb")
 
 db_uri = f"postgresql://{_db_user}:{_db_password}@{_db_host}/{_db_name}"
 db_pool_timeout = int(os.getenv("BASELINE_DB_POOL_TIMEOUT", "5"))
