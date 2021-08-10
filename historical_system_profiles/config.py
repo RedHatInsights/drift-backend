659fc7827d68370805e50a3b0a6867f88f9a8d56
Joao Paulo Ramos <joao.oramos11@gmail.com>
2021-08-10 09:24:39 -0300

    Clowderized HSP and updated Kerlescan

--- historical_system_profiles/config.py
+++ historical_system_profiles/config.py
@@ -1,7 +1,33 @@
 import logging
 import os
 
+from app_common_python import KafkaTopics, LoadedConfig, isClowderEnabled
 from kerlescan.config import str_to_bool
+
+
+def load_db_setting(env_name, attribute, default):
+    if isClowderEnabled():
+        cfg = LoadedConfig
+
+        return vars(cfg.database)[attribute]
+
+    return os.getenv(env_name, default)
+
+
+def load_kakfa_setting(env_name, default):
+    if isClowderEnabled():
+        cfg = LoadedConfig
+
+        broker_cfg = cfg.kafka.brokers[0]
+        return f"{broker_cfg.hostname}:{broker_cfg.port}"
+
+    return os.getenv(env_name, default).split(",")
+
+
+def topic(topic):
+    if isClowderEnabled():
+        return KafkaTopics[topic].name
+    return topic
 
 
 # pull the app name from the env var; we are not fully initialized yet
@@ -10,21 +36,21 @@ logger = logging.getLogger(app_name)
 
 
 # please ensure these are all documented in README.md
-_db_user = os.getenv("HSP_DB_USER", "insights")
-_db_password = os.getenv("HSP_DB_PASS", "insights")
-_db_host = os.getenv("HSP_DB_HOST", "localhost:5432")
-_db_name = os.getenv("HSP_DB_NAME", "insights")
+_db_user = load_db_setting("HSP_DB_USER", "username", "insights")
+_db_password = load_db_setting("HSP_DB_PASS", "password", "insights")
+_db_host = load_db_setting("HSP_DB_HOST", "hostname", "localhost:5432")
+_db_name = load_db_setting("HSP_DB_NAME", "name", "insights")
 
 db_uri = f"postgresql://{_db_user}:{_db_password}@{_db_host}/{_db_name}"
 db_pool_timeout = int(os.getenv("HSP_DB_POOL_TIMEOUT", "5"))
 db_pool_size = int(os.getenv("HSP_DB_POOL_SIZE", "5"))
 
-bootstrap_servers = os.getenv("BOOTSTRAP_SERVERS", "kafka:29092").split(",")
+bootstrap_servers = load_kakfa_setting("BOOTSTRAP_SERVERS", "kafka:29092")
 consume_topic = os.getenv("CONSUME_TOPIC", None)
 listener_type = os.getenv("LISTENER_TYPE", "ARCHIVER")
 kafka_group_id = os.getenv("KAFKA_GROUP_ID", "hsp-%s" % listener_type.lower())
-notification_service_topic = os.getenv(
-    "NOTIFICATION_SERVICE_TOPIC", "platform.notifications.ingress"
+notification_service_topic = topic(
+    os.getenv("NOTIFICATION_SERVICE_TOPIC", "platform.notifications.ingress")
 )
 notification_bundle = os.getenv("NOTIFICATION_BUNDLE", "rhel")
 notification_app = os.getenv("NOTIFICATION_APP", "drift")
@@ -44,7 +70,7 @@ hostname = os.getenv("HOSTNAME", "hsp-hostname-not-set")
 
 valid_profile_age_days = float(os.getenv("VALID_PROFILE_AGE_DAYS", 7.0))
 expired_cleaner_sleep_minutes = float(os.getenv("EXPIRED_CLEANER_SLEEP_MINUTES", 20.0))
-tracker_topic = os.getenv("TRACKER_TOPIC", "platform.payload-status")
+tracker_topic = topic(os.getenv("TRACKER_TOPIC", "platform.payload-status"))
 listener_metrics_port = int(os.getenv("LISTENER_METRICS_PORT", 5000))
 listener_delay = int(os.getenv("LISTENER_DELAY", 5))
 kafka_max_poll_interval_ms = int(os.getenv("KAFKA_MAX_POLL_INTERVAL_MS", 300000))
--- run_app.sh
+++ run_app.sh
@@ -1,33 +1,72 @@
-TEMPDIR=`mktemp -d`
+if [ "$ACG_CONFIG" ]
+then
+  echo "Found ACG_CONFIG - RUNNING WITH CLOWDER"
+  if [ "$IS_MIGRATION" ]
+  then
+    echo "RUNNING DB MIGRATION"
+    PORT=8000
+    APP_CONFIG='gunicorn.conf.py'
+    bash -c "FLASK_APP=historical_system_profiles.app:get_flask_app_with_migration flask db upgrade"
 
-if [ -z "$LOG_LEVEL" ];
-  then LOG_LEVEL='info'
+  else
+    if [ -z "$SERVICE_MODE" ];
+      then SERVICE_MODE=REST_API
+    fi
+    if [ "$SERVICE_MODE" == "REST_API" ]
+    then
+      echo "RUNNING BACKEND SERVICE"
+      PORT=8000
+      APP_CONFIG='gunicorn.conf.py'
+      exec gunicorn wsgi --bind=0.0.0.0:$PORT --access-logfile=- --config "$APP_CONFIG"
+    elif [ "$SERVICE_MODE" == "CLEAN_EXPIRED_RECORDS" ];
+      then
+      echo "RUNNING CLEAN_EXPIRED_RECORDS"
+      python clean_expired_records.py
+    elif [ "$LISTENER_TYPE" == "ARCHIVER" ];
+      then
+      echo "RUNNING ARCHIVER"
+      python kafka_listener.py
+    elif [ "$LISTENER_TYPE" == "DELETER" ];
+      then
+      echo "RUNNING DELETER"
+      python kafka_listener.py
+    fi
+  fi
+else
+  echo "Did not found ACG_CONFIG - RUNNING LOCALLY"
+
+  TEMPDIR=`mktemp -d`
+
+  if [ -z "$LOG_LEVEL" ];
+    then LOG_LEVEL='info'
+  fi
+
+  if [ -z "$NUM_WORKERS" ];
+    then NUM_WORKERS=2
+  fi
+
+  if [ -z "$THREADS" ];
+    then THREADS=2
+  fi
+
+  if [ -z "$PORT" ];
+    then PORT=8080
+  fi
+
+  if [ -z "$SERVICE_MODE" ];
+    then SERVICE_MODE=REST_API
+  fi
+
+  if [ "$SERVICE_MODE" == "REST_API" ];
+    then prometheus_multiproc_dir=$TEMPDIR gunicorn wsgi -w $NUM_WORKERS --threads $THREADS -b 0.0.0.0:$PORT --log-level=$LOG_LEVEL --access-logfile=- --config ./gunicorn.conf.py
+  elif [ "$SERVICE_MODE" == "CLEAN_EXPIRED_RECORDS" ];
+    then prometheus_multiproc_dir=$TEMPDIR python clean_expired_records.py
+  elif [ "$LISTENER_TYPE" == "ARCHIVER" ];
+    then python kafka_listener.py
+  elif [ "$LISTENER_TYPE" == "DELETER" ];
+    then python kafka_listener.py
+  fi
+
+  rm -rf $TEMPDIR
+
 fi
-
-if [ -z "$NUM_WORKERS" ];
-  then NUM_WORKERS=2
-fi
-
-if [ -z "$THREADS" ];
-  then THREADS=2
-fi
-
-if [ -z "$PORT" ];
-  then PORT=8080
-fi
-
-if [ -z "$SERVICE_MODE" ];
-  then SERVICE_MODE=REST_API
-fi
-
-if [ "$SERVICE_MODE" == "REST_API" ];
-  then prometheus_multiproc_dir=$TEMPDIR gunicorn wsgi -w $NUM_WORKERS --threads $THREADS -b 0.0.0.0:$PORT --log-level=$LOG_LEVEL --access-logfile=- --config ./gunicorn.conf.py
-elif [ "$SERVICE_MODE" == "CLEAN_EXPIRED_RECORDS" ];
-  then prometheus_multiproc_dir=$TEMPDIR python clean_expired_records.py
-elif [ "$LISTENER_TYPE" == "ARCHIVER" ];
-  then python kafka_listener.py
-elif [ "$LISTENER_TYPE" == "DELETER" ];
-  then python kafka_listener.py
-fi
-
-rm -rf $TEMPDIR
