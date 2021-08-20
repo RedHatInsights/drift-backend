a8d2d985fc33e7c5d11c8f152cd4fbe63bf059c8
Joao Paulo Ramos <joao.oramos11@gmail.com>
2021-08-20 12:00:12 -0300

    Implement pr_check.sh script to run with Jenkins

--- pr_check.sh
+++ pr_check.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
-set -exv
+#Adapted from https://github.com/RedHatInsights/insights-ingress-go/blob/master/pr_check.sh
 
-# Make directory for artifacts
-mkdir -p artifacts
+# --------------------------------------------
+# Options that must be configured by app owner
+# --------------------------------------------
+APP_NAME="drift"  # name of app-sre "application" folder this component lives in
+COMPONENT_NAME="system-baseline"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
+IMAGE="quay.io/cloudservices/system-baseline-backend"
 
-cat << EOF > artifacts/junit-dummy.xml
-<testsuite tests="1">
-    <testcase classname="dummy" name="dummytest"/>
-</testsuite>
-EOF
+IQE_PLUGINS="drift"
+IQE_MARKER_EXPRESSION="smoke" # Need to check this
+IQE_FILTER_EXPRESSION=""
+IQE_CJI_TIMEOUT="30m"
+
+
+# Install bonfire repo/initialize
+CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
+curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
+source $CICD_ROOT/build.sh
+source $APP_ROOT/ephemeral_unit_test.sh
+source $APP_ROOT/baseline_deploy_ephemeral_env.sh
+source $CICD_ROOT/cji_smoke_test.sh
