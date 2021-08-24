021551c38c268b0414485988aabe3ac0ee307921
Joao Paulo Ramos <joao.oramos11@gmail.com>
2021-08-24 11:27:44 -0300

    Fix IMAGE_NAME tag

--- build_deploy.sh
+++ build_deploy.sh
@@ -29,8 +29,8 @@ else
     export REGISTRY_AUTH_FILE="$AUTH_CONF_DIR/auth.json"
     podman login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
     podman login -u="$RH_REGISTRY_USER" -p="$RH_REGISTRY_TOKEN" registry.redhat.io
-    podman build -t "${IMAGE}:${IMAGE_TAG}" .
-    podman push "${IMAGE}:${IMAGE_TAG}"
+    podman build -t "${IMAGE_NAME}:${IMAGE_TAG}" .
+    podman push "${IMAGE_NAME}:${IMAGE_TAG}"
     for TAG in "latest" "qa"; do
         podman tag "${IMAGE_NAME}:${IMAGE_TAG}" "${IMAGE_NAME}:$TAG"
         podman push "${IMAGE_NAME}:$TAG"
