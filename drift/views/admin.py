3275bbf6da28f2c171199723f0d8414dd7ab6951
Fellipe Henrique <fpedrosa@redhat.com>
2021-08-24 09:31:07 -0300

    NO-TICKET Fix decorator on admin endpoint
    Fix wrong decorator on admin endpoint
    
    Signed-off-by: Fellipe Henrique <fpedrosa@redhat.com>

--- drift/views/admin.py
+++ drift/views/admin.py
@@ -7,7 +7,6 @@ from drift.baseline_service_interface import call_baseline_admin_svc
 section = Blueprint("internal_admin", __name__)
 
 
-@section.before_app_request
 def status():
     auth_key = get_key_from_headers(request.headers)
     result = call_baseline_admin_svc(
