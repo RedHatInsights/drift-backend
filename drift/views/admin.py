d535cf8fa0b114f756fb17a1ee8dbf107d78a339
Fellipe Henrique <fpedrosa@redhat.com>
2021-08-25 17:05:02 -0300

    DRFT-774 Fix auth from turnpike
    Allow `/admin/status` endoint be reacheable only from turnpike
    
    Signed-off-by: Fellipe Henrique <fpedrosa@redhat.com>

--- drift/views/admin.py
+++ drift/views/admin.py
@@ -1,4 +1,10 @@
+import base64
+import json
+
+from http import HTTPStatus
+
 from flask import Blueprint, current_app, request
+from kerlescan.exceptions import HTTPError
 from kerlescan.service_interface import get_key_from_headers
 
 from drift.baseline_service_interface import call_baseline_admin_svc
@@ -7,9 +13,21 @@ from drift.baseline_service_interface import call_baseline_admin_svc
 section = Blueprint("internal_admin", __name__)
 
 
-def status():
+def _check_turnpike_headers(**kwargs):
     auth_key = get_key_from_headers(request.headers)
-    result = call_baseline_admin_svc(
-        endpoint="status", service_auth_key=auth_key, logger=current_app.logger
-    )
-    return result
+
+    auth = json.loads(base64.b64decode(auth_key))
+    identity_type = auth.get("identity", {}).get("type", None)
+
+    return identity_type == "Associate"
+
+
+def status():
+    if _check_turnpike_headers(request=request):
+        auth_key = get_key_from_headers(request.headers)
+        result = call_baseline_admin_svc(
+            endpoint="status", service_auth_key=auth_key, logger=current_app.logger
+        )
+        return result
+
+    raise HTTPError(HTTPStatus.FORBIDDEN, message="Access Denied.")
