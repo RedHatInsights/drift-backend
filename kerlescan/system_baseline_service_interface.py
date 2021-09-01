0850fdbf1e12d06f9a948421e88d46673df07116
Petr Blaho <petrblaho@redhat.com>
2021-09-01 18:00:50 +0200

    Adds internal auth header to delete systems from notifications

--- kerlescan/system_baseline_service_interface.py
+++ kerlescan/system_baseline_service_interface.py
@@ -2,7 +2,7 @@ from urllib.parse import urljoin
 
 from kerlescan import config
 from kerlescan.constants import AUTH_HEADER_NAME, INTERNAL_BASELINE_SVC_DELETE_SYSTEM_ENDPOINT
-from kerlescan.service_interface import fetch_url
+from kerlescan.service_interface import fetch_url, internal_auth_header
 
 
 def delete_systems_from_notifications(
@@ -12,7 +12,7 @@ def delete_systems_from_notifications(
     deletes systems from associations for notifications at system baseline service
     """
 
-    auth_header = {AUTH_HEADER_NAME: service_auth_key}
+    auth_header = {**{AUTH_HEADER_NAME: service_auth_key}, **internal_auth_header()}
 
     deletion_request_location = urljoin(
         config.baseline_svc_hostname, INTERNAL_BASELINE_SVC_DELETE_SYSTEM_ENDPOINT
