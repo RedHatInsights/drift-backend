f1ccb57d7fa09e10859dbba9d30c733196bc27ce
Petr Blaho <petrblaho@redhat.com>
2021-09-15 18:54:15 +0200

    Uses path params not query params with internal systems

--- kerlescan/constants.py
+++ kerlescan/constants.py
@@ -16,7 +16,7 @@ INTERNAL_BASELINE_SVC_ENDPOINT = (
     "/api/system-baseline/internal/v1/baselines?system_id=%s"  # note missing trailing slash
 )
 INTERNAL_BASELINE_SVC_DELETE_SYSTEM_ENDPOINT = (
-    "/api/system-baseline/internal/v1/systems?system_ids[]=%s"
+    "/api/system-baseline/internal/v1/systems/%s"
 )
 SYSTEM_ID_KEY = "id"
 
