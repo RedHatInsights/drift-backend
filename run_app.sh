bab17165851c3555540698071f551ca46594aa46
Joao Paulo Ramos <jramos@redhat.com>
2021-10-08 14:35:42 -0300

    Add exit code if DB migration fails to run

--- run_app.sh
+++ run_app.sh
@@ -11,7 +11,8 @@ then
     PORT=8000
     METRICS_PORT=9000
     APP_CONFIG='gunicorn.conf.py'
-    bash -c "FLASK_APP=historical_system_profiles.app:get_flask_app_with_migration flask db upgrade"
+    FLASK_APP=historical_system_profiles.app:get_flask_app_with_migration flask db upgrade;
+    if [[ "$?" != "0" ]]; then exit 1; fi
     exec gunicorn wsgi --bind=0.0.0.0:$PORT --bind=0.0.0.0:$METRICS_PORT --access-logfile=- --config "$APP_CONFIG"
   elif [ "$SERVICE_MODE" == "CLEAN_EXPIRED_RECORDS" ];
     then
