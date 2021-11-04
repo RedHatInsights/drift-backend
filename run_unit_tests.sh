9750daa396faed88553cd5de8c32122f13832697
Joao Paulo Ramos <jramos@redhat.com>
2021-11-04 08:01:09 -0300

    Testing feature flags

--- run_unit_tests.sh
+++ run_unit_tests.sh
@@ -4,5 +4,6 @@
 # 'prometheus_multiproc_dir' is set up and torn down.
 
 TEMPDIR=`mktemp -d`
+export UNLEASH_TOKEN="token"
 
 prometheus_multiproc_dir=$TEMPDIR pytest "$@" && prometheus_multiproc_dir=$TEMPDIR python generate_report.py test_reports.toml && rm -rf $TEMPDIR
