f7a69007ab66a79b7e68b8f66b60ee868ec55473
Joao Paulo Ramos <jramos@redhat.com>
2021-11-24 09:43:28 -0300

    Add missing bearer keyword in token

--- kerlescan/unleash.py
+++ kerlescan/unleash.py
@@ -11,21 +11,24 @@ def initUnleash():
         UNLEASH_TOKEN=os.getenv("UNLEASH_TOKEN"),
     )
     UNLEASH_URL = "http://unleash_svc_url_is_not_set"
+    UNLEASH_TOKEN = None
     unleash = LoadedConfig.featureFlags
     if unleash:
         UNLEASH_URL = f"{unleash.hostname}:{unleash.port}/api"
         if unleash.port == 443:
             UNLEASH_URL = f"https://{UNLEASH_URL}"
+            UNLEASH_TOKEN = unleash.clientAccessToken
         else:
             UNLEASH_URL = f"http://{UNLEASH_URL}"
+            UNLEASH_TOKEN = unleash.clientAccessToken
 
     configs["UNLEASH_URL"] = configs["UNLEASH_URL"] or UNLEASH_URL
-    configs["UNLEASH_TOKEN"] = configs["UNLEASH_TOKEN"] or unleash.clientAccessToken
+    configs["UNLEASH_TOKEN"] = configs["UNLEASH_TOKEN"] or UNLEASH_TOKEN
 
     client = UnleashClient(
         url=configs["UNLEASH_URL"],
         app_name="drift",
-        custom_headers={"Authorization": configs["UNLEASH_TOKEN"]},
+        custom_headers={"Authorization": f'Bearer {configs["UNLEASH_TOKEN"]}'},
     )
     client.initialize_client()
     return client
