1ce4575ea7a3ae295a3091bc86e7413962d7d543
Michael Johnson <micjohns@newbeginnings.usersys.redhat.com>
2022-01-10 17:06:48 -0500

    Update init_ephemeral_cluster.sh for bonfire v3

--- clowder/init_ephemeral_cluster.sh
+++ clowder/init_ephemeral_cluster.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env
 
 # See step 7 of this link: https://clouddot.pages.redhat.com/docs/dev/getting-started/ephemeral/onboarding.html
-echo "Loggin to Openshift cluster"
+echo "Logging into Openshift cluster"
 sh ~/ephemeral-login.sh
 echo "Checking for reserved namespace"
 export NAMESPACE=$(bonfire namespace list --mine | grep "ephemeral" | awk '{print $1"|"$2}' | grep '|true' | awk -F'|' '{print $1}' | head -n1) # we use the first one available
 
 if [[ $NAMESPACE == *"ephemeral-"* ]]; then
     echo "Namespace $NAMESPACE reserved, extending for 2 hours"    
-    bonfire namespace reserve -d 2 $NAMESPACE 
+    bonfire namespace extend $NAMESPACE -d '2h0m' 
 else
     echo "Reserving namespace for 2 hours"
-    export NAMESPACE=$(bonfire namespace reserve -d 2)
+    export NAMESPACE=$(bonfire namespace reserve -d '2h0m')
     echo "Namespace $NAMESPACE reserved"
 fi
 
@@ -19,6 +19,6 @@ echo "Using $NAMESPACE as default oc project"
 oc project $NAMESPACE
 
 echo "Deploying apps to $NAMESPACE"
-bonfire deploy drift xjoin-search -n $NAMESPACE
+bonfire deploy drift -n $NAMESPACE
 
 sh ./clowder/clowder-port-forward.sh
