6bfb5f4068afae6dfcd37bf3951ad4f601314ce7
Joao Paulo Ramos <jramos@redhat.com>
2022-01-13 15:59:52 -0300

    Consume cloudwatch configuration from Clowder

--- kerlescan/cloudwatch.py
+++ kerlescan/cloudwatch.py
@@ -2,6 +2,7 @@ import os
 
 import watchtower
 
+from app_common_python import LoadedConfig, isClowderEnabled
 from boto3.session import Session
 
 
@@ -11,17 +12,26 @@ def setup_cw_logging(logger):  # pragma: no cover
 
     from https://github.com/RedHatInsights/cloudwatch-test
     """
-    key_id = os.environ.get("CW_AWS_ACCESS_KEY_ID")
-    secret = os.environ.get("CW_AWS_SECRET_ACCESS_KEY")
+
+    if isClowderEnabled:
+        cloudwatch_cfg = LoadedConfig.logging.cloudwatch
+
+        key_id = cloudwatch_cfg.accessKeyId
+        secret = cloudwatch_cfg.secretAccessKey
+        region = cloudwatch_cfg.region
+        log_group = cloudwatch_cfg.logGroup
+
+    else:
+        key_id = os.environ.get("CW_AWS_ACCESS_KEY_ID")
+        secret = os.environ.get("CW_AWS_SECRET_ACCESS_KEY")
+        region = os.environ.get("AWS_REGION", "us-east-1")
+        log_group = os.environ.get("CW_LOG_GROUP", "platform-dev")
+
     if not (key_id and secret):
         logger.info("CloudWatch logging disabled due to missing access key")
         return
 
-    session = Session(
-        aws_access_key_id=key_id,
-        aws_secret_access_key=secret,
-        region_name=os.environ.get("AWS_REGION", "us-east-1"),
-    )
+    session = Session(aws_access_key_id=key_id, aws_secret_access_key=secret, region_name=region)
 
     try:
         with open("/var/run/secrets/kubernetes.io/serviceaccount/namespace", "r") as f:
@@ -31,7 +41,7 @@ def setup_cw_logging(logger):  # pragma: no cover
 
     handler = watchtower.CloudWatchLogHandler(
         boto3_session=session,
-        log_group=os.environ.get("CW_LOG_GROUP", "platform-dev"),
+        log_group=log_group,
         stream_name=namespace,
         create_log_group=False,
     )
