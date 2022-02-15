fd4a61ae76ec7fdcad11226358ea4c91c18c335a
Petr Blaho <petrblaho@redhat.com>
2022-02-15 15:40:33 +0100

    Prints out the bonfire config

--- pr_check.sh
+++ pr_check.sh
@@ -3,6 +3,9 @@
 #fetch our shared bonfire config
 #curl -o config.yaml https://raw.githubusercontent.com/RedHatInsights/drift-dev-setup/master/clowder/pr_check_bonfire_config.yaml
 
+#show bonfire config
+cat /var/lib/jenkins/.config/bonfire/config.yaml
+
 #Adapted from https://github.com/RedHatInsights/insights-ingress-go/blob/master/pr_check.sh
 
 # --------------------------------------------
