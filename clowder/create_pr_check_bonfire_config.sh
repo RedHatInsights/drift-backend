3c058b493b0cc261d1471b00fcb7783af0640e27
Petr Blaho <petrblaho@redhat.com>
2022-02-15 20:23:25 +0100

    Removes template, script and resulting shared bonfire config
    Current bonfire does not load config from working directory so we
    stopped using this in drift pr checks.

--- clowder/create_pr_check_bonfire_config.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-export NOTIFICATIONS_BACKEND__REF="master"
-export NOTIFICATIONS_GW__REF="main"
-
-CLOWDER_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
-echo $CLOWDER_DIR
-envsubst < $CLOWDER_DIR/pr_check_bonfire_config_template.yaml > $CLOWDER_DIR/pr_check_bonfire_config.yaml
