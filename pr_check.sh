dddf46e774b2a8afded04a72e4a910cd1125f680
Petr Blaho <petrblaho@redhat.com>
2022-02-15 20:05:10 +0100

    Removes shared bonfire config download
    Bonfire does not load config from current dir's config.yaml anymore.

--- pr_check.sh
+++ pr_check.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#fetch our shared bonfire config
-curl -o config.yaml https://raw.githubusercontent.com/RedHatInsights/drift-dev-setup/master/clowder/pr_check_bonfire_config.yaml
-
 #Adapted from https://github.com/RedHatInsights/insights-ingress-go/blob/master/pr_check.sh
 
 # --------------------------------------------
