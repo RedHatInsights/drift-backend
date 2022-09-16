ec7b71de69709894743c57859a642315c4895353
Petr Blaho <petrblaho@redhat.com>
2022-09-16 14:59:32 +0200

    Renames method for loading kafka brokers

--- historical_system_profiles/config.py
+++ historical_system_profiles/config.py
@@ -14,7 +14,7 @@ def load_db_setting(env_name, attribute, default):
     return os.getenv(env_name, default)
 
 
-def load_kafka_setting(env_name, default):
+def load_kafka_brokers(env_name, default):
     if isClowderEnabled():
         cfg = LoadedConfig
 
@@ -108,7 +108,7 @@ db_uri = f"postgresql://{_db_user}:{_db_password}@{_db_host}:{_db_port}/{_db_nam
 db_pool_timeout = int(os.getenv("HSP_DB_POOL_TIMEOUT", "5"))
 db_pool_size = int(os.getenv("HSP_DB_POOL_SIZE", "5"))
 
-bootstrap_servers = load_kafka_setting("BOOTSTRAP_SERVERS", "kafka:29092")
+bootstrap_servers = load_kafka_brokers("BOOTSTRAP_SERVERS", "kafka:29092")
 consume_topic = topic(os.getenv("CONSUME_TOPIC", "platform.inventory.events"))
 listener_type = os.getenv("LISTENER_TYPE", "ARCHIVER")
 kafka_group_id = os.getenv("KAFKA_GROUP_ID", "hsp-%s" % listener_type.lower())
