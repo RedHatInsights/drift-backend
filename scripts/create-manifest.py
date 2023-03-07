f57c693e72fbde0c089241bfaa3f99b3ba3eac47
Petr Blaho <petrblaho@redhat.com>
2023-03-07 17:10:30 +0100

    Updates create-manifest script to use poetry lockfile

--- scripts/create-manifest.py
+++ scripts/create-manifest.py
@@ -1,22 +1,41 @@
 #!/usr/bin/env python
 
-import json
-import os
+# import json
+# import os
+
+import tomli
 
 
-modified_time = int(os.path.getmtime("Pipfile.lock"))
 lockfile = {}
 
-with open("Pipfile.lock") as json_file:
-    lockfile = json.load(json_file)
+# pipenv
+# modified_time = int(os.path.getmtime("Pipfile.lock"))
+# with open("Pipfile.lock") as json_file:
+#     lockfile = json.load(json_file)
+
+# with open("drift-manifest", "w") as manifest:
+#     for name, value in sorted(lockfile["default"].items()):
+#         if "version" in value:
+#             version = value["version"].replace("=", "")
+#             manifest.write("services-drift:drift/python:3.8=%s:%s\n" % (name, version))
+#         elif "ref" in value:
+#             ref = value["ref"]
+#             manifest.write("services-drift:drift/python:3.8=%s:%s\n" % (name, ref))
+#         else:
+#             raise f"unable to parse {value}"
+
+# poetry
+with open("poetry.lock", "rb") as json_file:
+    lockfile = tomli.load(json_file)
 
 with open("drift-manifest", "w") as manifest:
-    for name, value in sorted(lockfile["default"].items()):
-        if "version" in value:
-            version = value["version"].replace("=", "")
+    for package in sorted(lockfile["package"], key=lambda k: k["name"]):
+        name = package["name"]
+        if "version" in package:
+            version = package["version"].replace("=", "")
             manifest.write("services-drift:drift/python:3.8=%s:%s\n" % (name, version))
-        elif "ref" in value:
-            ref = value["ref"]
+        elif "resolved_reference" in package["source"]:
+            ref = package["source"]["resolved_reference"]
             manifest.write("services-drift:drift/python:3.8=%s:%s\n" % (name, ref))
         else:
-            raise f"unable to parse {value}"
+            raise f"unable to parse {package}"
