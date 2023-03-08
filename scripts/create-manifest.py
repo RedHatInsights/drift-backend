440a65484358addba026e0d9cb4fe3370fee65c2
Petr Blaho <petrblaho@redhat.com>
2023-03-08 16:23:52 +0100

    Updates create-manifest script to use poetry lockfile

--- scripts/create-manifest.py
+++ scripts/create-manifest.py
@@ -1,24 +1,49 @@
 #!/usr/bin/env python
 
-import json
+# import json
+# import os
+
+import tomli
 
 
 lockfile = {}
 
-with open("Pipfile.lock") as json_file:
-    lockfile = json.load(json_file)
+# pipenv
+# modified_time = int(os.path.getmtime("Pipfile.lock"))
+# with open("Pipfile.lock") as json_file:
+#     lockfile = json.load(json_file)
+
+# with open("historical_system_profiles-manifest", "w") as manifest:
+#     for name, value in sorted(lockfile["default"].items()):
+#         if "version" in value:
+#             version = value["version"].replace("=", "")
+#             manifest.write(
+#                 "services-drift:historical_system_profiles/python:3.8=%s:%s\n" % (name, version)
+#             )
+#         elif "ref" in value:
+#             ref = value["ref"]
+#             manifest.write(
+#                 "services-drift:historical_system_profiles/python:3.8=%s:%s\n" % (name, ref)
+#             )
+#         else:
+#             raise f"unable to parse {value}"
+
+# poetry
+with open("poetry.lock", "rb") as json_file:
+    lockfile = tomli.load(json_file)
 
 with open("historical_system_profiles-manifest", "w") as manifest:
-    for name, value in sorted(lockfile["default"].items()):
-        if "version" in value:
-            version = value["version"].replace("=", "")
+    for package in sorted(lockfile["package"], key=lambda k: k["name"]):
+        name = package["name"]
+        if "version" in package:
+            version = package["version"].replace("=", "")
             manifest.write(
                 "services-drift:historical_system_profiles/python:3.8=%s:%s\n" % (name, version)
             )
-        elif "ref" in value:
-            ref = value["ref"]
+        elif "resolved_reference" in package["source"]:
+            ref = package["source"]["resolved_reference"]
             manifest.write(
                 "services-drift:historical_system_profiles/python:3.8=%s:%s\n" % (name, ref)
             )
         else:
-            raise "unable to parse %s" % value
+            raise f"unable to parse {package}"
