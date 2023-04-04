d668496f57fc82be6f2fbab001e5d697cb8fd03b
Petr Blaho <petrblaho@gmail.com>
2023-04-04 18:41:56 +0200

    Fails check-manifest when create-manifest fails

--- scripts/check-manifest.sh
+++ scripts/check-manifest.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 ./scripts/create-manifest.py
+if [ ! $? -eq 0 ]; then
+    exit 1
+fi
 
 changed=`git diff --name-only HEAD`
 
 if [[ $changed == *"drift-manifest"* ]]; then
-  echo "Pipfile.lock or poetry.lock changed without updating drift-manifest since latest commit. Run ./scripts/create-manifest.py to update drift-manifest, then commit."
+  echo "Pipfile.lock changed without updating system_baseline-manifest. Run ./scripts/create-manifest.py to update."
   exit 1
 else
   exit 0
