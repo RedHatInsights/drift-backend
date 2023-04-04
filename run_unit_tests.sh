b53e72c97d9761721d92a80bf4a6e52a82fc76f7
Petr Blaho <petrblaho@gmail.com>
2023-04-04 17:33:02 +0200

    Changes format of coverage report to xml

--- run_unit_tests.sh
+++ run_unit_tests.sh
@@ -6,4 +6,4 @@
 TEMPDIR=`mktemp -d`
 export UNLEASH_TOKEN="token"
 
-prometheus_multiproc_dir=$TEMPDIR pytest "$@" && prometheus_multiproc_dir=$TEMPDIR python generate_report.py test_reports.toml && rm -rf $TEMPDIR
+prometheus_multiproc_dir=$TEMPDIR pytest --cov-report xml "$@" && prometheus_multiproc_dir=$TEMPDIR python generate_report.py test_reports.toml && rm -rf $TEMPDIR
