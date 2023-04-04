890d0bfb85deeb381fa40f5b680ce9a9b8a3cd25
Petr Blaho <petrblaho@gmail.com>
2023-04-04 18:53:37 +0200

    Changes format of coverage report to xml

--- run_unit_tests.sh
+++ run_unit_tests.sh
@@ -10,7 +10,7 @@ psql 'postgresql://insights:insights@localhost:5432/baselinedb' -c 'create datab
 
 BASELINE_DB_NAME=testdb FLASK_APP=system_baseline.app:get_flask_app_with_migration flask db upgrade
 
-BASELINE_DB_NAME=testdb prometheus_multiproc_dir=$TEMPDIR pytest && rm -rf $TEMPDIR
+BASELINE_DB_NAME=testdb prometheus_multiproc_dir=$TEMPDIR pytest --cov-report xml && rm -rf $TEMPDIR
 
 result=$?
 
