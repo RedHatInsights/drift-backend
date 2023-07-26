70c7656c274529436519344ab669df4e7bc3254d
Petr Blaho <petrblaho@gmail.com>
2023-07-26 11:39:57 +0200

    Mocks fetch_url response instead of get_perms in RBAC test

--- tests/test_rbac.py
+++ tests/test_rbac.py
@@ -10,15 +10,15 @@ from kerlescan.exceptions import HTTPError
 
 class RBACTests(unittest.TestCase):
     @mock.patch("kerlescan.view_helpers.get_key_from_headers")
-    @mock.patch("kerlescan.view_helpers.get_perms")
-    def test_has_perm(self, mock_get_perms, mock_get_key):
+    @mock.patch("kerlescan.rbac_service_interface.fetch_url")
+    def test_has_perm(self, mock_fetch_url, mock_get_key):
         mock_get_key.return_value = (
             "eyJpZGVudGl0eSI6IHsiYWNjb3VudF9udW1iZXIiOiAiMTIxMjcyOSIsICJ0eXBlIjogI"
             "lN5c3RlbSIsICJhdXRoX3R5cGUiOiAiY2xhc3NpYy1wcm94eSIsICJzeXN0ZW0iOiB7Im"
             "NuIjogIjIyY2Q4ZTM5LTEzYmItNGQwMi04MzE2LTg0Yjg1MGRjNTEzNiIsICJjZXJ0X3R"
             "5cGUiOiAic3lzdGVtIn0sICJpbnRlcm5hbCI6IHsib3JnX2lkIjogIjAwMDAwMSJ9fX0K"
         )
-        mock_get_perms.return_value = ["myperm:*:*"]
+        mock_fetch_url.return_value = {"data": [{"permission": "myperm:*:*"}]}
         mock_request = mm()
         mock_request.path = "/some/path"
         self.assertIsNone(
@@ -34,15 +34,15 @@ class RBACTests(unittest.TestCase):
         )
 
     @mock.patch("kerlescan.view_helpers.get_key_from_headers")
-    @mock.patch("kerlescan.view_helpers.get_perms")
-    def test_missing_perm(self, mock_get_perms, mock_get_key):
+    @mock.patch("kerlescan.rbac_service_interface.fetch_url")
+    def test_missing_perm(self, mock_fetch_url, mock_get_key):
         mock_get_key.return_value = (
             "eyJpZGVudGl0eSI6IHsiYWNjb3VudF9udW1iZXIiOiAiMTIxMjcyOSIsICJ0eXBlIjogI"
             "lN5c3RlbSIsICJhdXRoX3R5cGUiOiAiY2xhc3NpYy1wcm94eSIsICJzeXN0ZW0iOiB7Im"
             "NuIjogIjIyY2Q4ZTM5LTEzYmItNGQwMi04MzE2LTg0Yjg1MGRjNTEzNiIsICJjZXJ0X3R"
             "5cGUiOiAic3lzdGVtIn0sICJpbnRlcm5hbCI6IHsib3JnX2lkIjogIjAwMDAwMSJ9fX0K"
         )
-        mock_get_perms.return_value = ["myperm:*:*"]
+        mock_fetch_url.return_value = {"data": [{"permission": "myperm:*:*"}]}
         mock_request = mm()
         mock_request.path = "/some/path"
         with self.assertRaises(HTTPError):
