57b24963b784211c0c5a583c4b5701d12f9cd061
Petr Blaho <petrblaho@gmail.com>
2023-07-31 16:41:10 +0200

    Adds parser for RBAC's inventory group ids

--- kerlescan/rbac_service_interface.py
+++ kerlescan/rbac_service_interface.py
@@ -18,3 +18,76 @@ def get_perms(application, service_auth_key, logger, request_metric, exception_m
     perms = [perm["permission"] for perm in rbac_result["data"]]
 
     return perms
+
+
+def get_rbac_filters(rbac_data):
+    """Gets RBAC filters from RBAC data"""
+    # possible values in "groups.id" key:
+    # None - no filter applied, all hosts allowed
+    # [] - no groups ids present, no hosts allowed
+    # [id1, id2, None] - group ids present, only hosts with id1, id2 or no group (None) allowed
+
+    # TODO: select all permisions needed
+    permission_list = [
+        "inventory:hosts:read",
+        "inventory:hosts:*",
+        "inventory:*:read",
+        "inventory:*:*",
+    ]
+
+    # default result is to allow no hosts
+    group_ids = []
+
+    def get_attfibute_filter_value(filter):
+        value = filter.get("value", None)
+        if value is None:
+            return value
+        elif isinstance(value, list):
+            return value
+        elif isinstance(value, str):
+            # split by comma, parses "null" as None
+            return [None if val == "null" else val for val in value.split(",")]
+        else:
+            raise TypeError
+
+    # get only relevant permission records
+    definitions = [
+        perm["resourceDefinitions"] for perm in rbac_data if perm["permission"] in permission_list
+    ]
+
+    # if definition contains []
+    # then we have empty resource definition for allowed permission thus we allow all hosts
+    if [] in definitions:
+        group_ids = None
+
+    # flatten definitions so we can work with all of them at once
+    definitions = [item for row in definitions for item in row]
+
+    if group_ids is not None:
+        # no definition means no matched permission
+        if definitions == []:
+            group_ids = []
+        else:
+            # get attributeFilters relevant for inventory groups
+            attribute_filters = [
+                definition.get("attributeFilter", {})
+                for definition in definitions
+                if definition.get("attributeFilter", {}).get("key") == "group.id"
+            ]
+
+            if attribute_filters == []:
+                group_ids = None
+            else:
+                group_ids = [
+                    get_attfibute_filter_value(attribute_filter)
+                    for attribute_filter in attribute_filters
+                ]
+                # flatten them
+                if None in group_ids:
+                    group_ids = None
+                else:
+                    group_ids = [item for row in group_ids for item in row]
+
+    result = {"group.id": group_ids}
+
+    return result
--- tests/test_rbac.py
+++ tests/test_rbac.py
@@ -6,6 +6,7 @@ from mock import MagicMock as mm
 
 from kerlescan import view_helpers
 from kerlescan.exceptions import HTTPError
+from kerlescan.rbac_service_interface import get_rbac_filters
 
 
 class RBACTests(unittest.TestCase):
@@ -55,3 +56,242 @@ class RBACTests(unittest.TestCase):
                 request_metric=mm(),
                 exception_metric=mm(),
             )
+
+    def test_get_rbac_filters_no_inventory_permission(self):
+        rbac_data = [
+            {"resourceDefinitions": [], "permission": "advisor:*:*"},
+        ]
+        rbac_filters = get_rbac_filters(rbac_data)
+        self.assertEqual(rbac_filters, {"group.id": []})  # no hosts
+
+    def test_get_rbac_filters_read_permisson_empty_resource(self):
+        rbac_data = [
+            {"resourceDefinitions": [], "permission": "inventory:hosts:read"},
+        ]
+        rbac_filters = get_rbac_filters(rbac_data)
+        self.assertEqual(rbac_filters, {"group.id": None})  # all hosts
+
+    def test_get_rbac_filters_only_write_permisson(self):
+        rbac_data = [
+            {"resourceDefinitions": [], "permission": "inventory:groups:write"},
+        ]
+        rbac_filters = get_rbac_filters(rbac_data)
+        self.assertEqual(rbac_filters, {"group.id": []})  # no hosts
+
+    def test_get_rbac_filters_read_permisson_not_relevant_resource(self):
+        rbac_data = [
+            {
+                "resourceDefinitions": [
+                    {
+                        "attributeFilter": {
+                            "key": "other.id",
+                            "value": ["foo", "bar"],
+                            "operation": "in",
+                        }
+                    }
+                ],
+                "permission": "inventory:hosts:read",
+            },
+        ]
+        rbac_filters = get_rbac_filters(rbac_data)
+        self.assertEqual(
+            rbac_filters,
+            {"group.id": None},
+        )  # all hosts
+
+    def test_get_rbac_filters_read_permisson_no_value_resource(self):
+        rbac_data = [
+            {
+                "resourceDefinitions": [
+                    {
+                        "attributeFilter": {
+                            "key": "group.id",
+                            "operation": "in",
+                        }
+                    }
+                ],
+                "permission": "inventory:hosts:read",
+            },
+        ]
+        rbac_filters = get_rbac_filters(rbac_data)
+        self.assertEqual(
+            rbac_filters,
+            {"group.id": None},
+        )
+
+    def test_get_rbac_filters_read_permisson_complex_resource(self):
+        rbac_data = [
+            {
+                "resourceDefinitions": [
+                    {
+                        "attributeFilter": {
+                            "key": "group.id",
+                            "value": [
+                                "df57820e-965c-49a6-b0bc-797b7dd60581",  # Group 1
+                                "df3f0efd-c853-41b5-80a1-86881d5343d1",  # Group 2
+                                None,  # No group (ungrouped hosts)
+                            ],
+                            "operation": "in",
+                        }
+                    }
+                ],
+                "permission": "inventory:hosts:read",
+            },
+        ]
+        rbac_filters = get_rbac_filters(rbac_data)
+        self.assertEqual(
+            rbac_filters,
+            {
+                "group.id": [
+                    "df57820e-965c-49a6-b0bc-797b7dd60581",
+                    "df3f0efd-c853-41b5-80a1-86881d5343d1",
+                    None,
+                ]
+            },
+        )
+
+    def test_get_rbac_filters_multiple_read_permissons(self):
+        rbac_data = [
+            {
+                "resourceDefinitions": [
+                    {
+                        "attributeFilter": {
+                            "key": "group.id",
+                            "value": [
+                                "df57820e-965c-49a6-b0bc-797b7dd60581",  # Group 1
+                            ],
+                            "operation": "in",
+                        }
+                    }
+                ],
+                "permission": "inventory:hosts:read",
+            },
+            {
+                "resourceDefinitions": [
+                    {
+                        "attributeFilter": {
+                            "key": "group.id",
+                            "value": [
+                                "df3f0efd-c853-41b5-80a1-86881d5343d1",  # Group 2
+                            ],
+                            "operation": "in",
+                        }
+                    }
+                ],
+                "permission": "inventory:hosts:read",
+            },
+        ]
+        rbac_filters = get_rbac_filters(rbac_data)
+        self.assertEqual(
+            rbac_filters,
+            {
+                "group.id": [
+                    "df57820e-965c-49a6-b0bc-797b7dd60581",
+                    "df3f0efd-c853-41b5-80a1-86881d5343d1",
+                ]
+            },
+        )
+
+    def test_get_rbac_filters_with_all_groups_permissons(self):
+        rbac_data = [
+            {
+                "resourceDefinitions": [
+                    {
+                        "attributeFilter": {
+                            "key": "group.id",
+                            "value": [
+                                "df57820e-965c-49a6-b0bc-797b7dd60581",  # Group 1
+                            ],
+                            "operation": "in",
+                        }
+                    }
+                ],
+                "permission": "inventory:hosts:read",
+            },
+            {
+                "resourceDefinitions": [
+                    {
+                        "attributeFilter": {
+                            "key": "group.id",
+                            "operation": "in",
+                        }
+                    }
+                ],
+                "permission": "inventory:hosts:read",
+            },
+        ]
+        rbac_filters = get_rbac_filters(rbac_data)
+        self.assertEqual(
+            rbac_filters,
+            {"group.id": None},
+        )
+
+    def test_get_rbac_filters_value_string(self):
+        rbac_data = [
+            {
+                "resourceDefinitions": [
+                    {
+                        "attributeFilter": {
+                            "key": "group.id",
+                            "value": "df57820e-965c-49a6-b0bc-797b7dd60581,df3f0efd-c853-41b5-80a1-86881d5343d1",
+                            "operation": "in",
+                        }
+                    }
+                ],
+                "permission": "inventory:hosts:read",
+            },
+        ]
+        rbac_filters = get_rbac_filters(rbac_data)
+        self.assertEqual(
+            rbac_filters,
+            {
+                "group.id": [
+                    "df57820e-965c-49a6-b0bc-797b7dd60581",
+                    "df3f0efd-c853-41b5-80a1-86881d5343d1",
+                ]
+            },
+        )
+
+    def test_get_rbac_filters_value_string_with_null(self):
+        rbac_data = [
+            {
+                "resourceDefinitions": [
+                    {
+                        "attributeFilter": {
+                            "key": "group.id",
+                            "value": "df57820e-965c-49a6-b0bc-797b7dd60581,df3f0efd-c853-41b5-80a1-86881d5343d1,null",
+                            "operation": "in",
+                        }
+                    }
+                ],
+                "permission": "inventory:hosts:read",
+            },
+        ]
+        rbac_filters = get_rbac_filters(rbac_data)
+        self.assertEqual(
+            rbac_filters,
+            {
+                "group.id": [
+                    "df57820e-965c-49a6-b0bc-797b7dd60581",
+                    "df3f0efd-c853-41b5-80a1-86881d5343d1",
+                    None,
+                ]
+            },
+        )
+
+    def test_get_rbac_filters_value_unsupported(self):
+        rbac_data = [
+            {
+                "resourceDefinitions": [
+                    {
+                        "attributeFilter": {
+                            "key": "group.id",
+                            "value": {},
+                            "operation": "in",
+                        }
+                    }
+                ],
+                "permission": "inventory:hosts:read",
+            },
+        ]
+        self.assertRaises(TypeError, get_rbac_filters, rbac_data)
