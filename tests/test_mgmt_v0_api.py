68cce43b1edff9be0e9e97e309c2351e35421a10
Petr Blaho <petrblaho@gmail.com>
2023-11-06 15:53:25 +0100

    chore: Connexion 3.0 update related changes
    RHINENG-2950

--- tests/test_mgmt_v0_api.py
+++ tests/test_mgmt_v0_api.py
@@ -7,14 +7,13 @@ from drift import app
 class ManagementApiTests(unittest.TestCase):
     def setUp(self):
         test_connexion_app = app.create_app()
-        test_flask_app = test_connexion_app.app
-        self.client = test_flask_app.test_client()
+        self.client = test_connexion_app.test_client()
 
     def test_status(self):
         response = self.client.get("mgmt/v0/status")
 
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(json.loads(response.data), {"status": "running"})
+        self.assertEqual(json.loads(response.content), {"status": "running"})
 
     def test_metrics(self):
         response = self.client.get("mgmt/v0/metrics")
