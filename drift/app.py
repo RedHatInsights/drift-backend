0202ad0d5fe7dce5474023bc9e29f6208961d97c
Janos Laszlo Vasik <jvasik@redhat.com>
2024-03-22 12:04:11 +0100

    feat: add ContextMiddleware

--- drift/app.py
+++ drift/app.py
@@ -2,6 +2,7 @@ import logging
 import os
 
 from connexion import FlaskApp
+from connexion.middleware.context import ContextMiddleware
 from kerlescan import config
 from kerlescan.audit_logging import setup_audit_logging
 from kerlescan.cloudwatch import setup_cw_logging
@@ -22,7 +23,8 @@ def create_app():
     app_name = os.getenv("APP_NAME", "drift")
     openapi_args = {"path_prefix": config.path_prefix, "app_name": app_name}
     connexion_app = FlaskApp(__name__, specification_dir="openapi/", arguments=openapi_args)
-    # connexion_app.add_api("api.spec.yaml", validate_responses=True, strict_validation=True)
+    connexion_app.add_middleware(ContextMiddleware, **options)
+    # FIXME: response validation is turned off for now
     connexion_app.add_api("api.spec.yaml", strict_validation=True)
     connexion_app.add_api("mgmt_api.spec.yaml")
     connexion_app.add_api("admin_api.spec.yaml", validate_responses=True, strict_validation=True)
